a2c271f0d091963dd142b403abf193c046a93959
Wasif-khan19 <wk5599426@gmail.com>
2024-11-20 19:10:43 +0500

    new

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  
+  return (
+    <>
+      <h1 className='text-6xl font-black'>
+        Muhammad Wasif
+        <Button>Muhammad</Button>
+      </h1>
+    </>
+  )
+}
+
+export default App
